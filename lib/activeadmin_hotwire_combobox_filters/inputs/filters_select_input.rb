e85195c30f90ea4923cb11af1d426edd726e2ab2
batman <batmanartiller@gmail.com>
2025-11-24 00:24:54 +0200

    fix undefined current_user method error

--- lib/activeadmin_hotwire_combobox_filters/inputs/filters_select_input.rb
+++ lib/activeadmin_hotwire_combobox_filters/inputs/filters_select_input.rb
@@ -6,7 +6,7 @@ ActiveAdmin::Inputs::Filters::SelectInput.class_eval do
 
     options = []
     if current_value.present?
-      current_user = send(ActiveAdmin.application.current_user_method)
+      current_user = template.active_admin_authorization.user
       value_record = Pundit.policy_scope!(current_user, reflection.klass).find_by(id: current_value)
       options = HotwireCombobox::Listbox::Item.collection_for(
         template, [value_record], render_in: {}, include_blank: nil, display: :to_combobox_display
